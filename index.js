82cb643ae69bda5f5f286bfac6751e5c54202938
Subirdas29 <dasjoy104@gmail.com>
2022-10-27 11:19:26 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
+app.get('/courses',(req,res)=>
+{
+  res.send(courses);
+})
+
+app.get('/course/:id',(req,res)=>
+{
+  const id = req.params.id;
+  const select_course = courses.find(course => course.id===id)
+  res.send(select_course)
+console.log(req.params)
+})
+
 app.get('/', (req, res) => {
     res.send('eduskill courses')
   })
